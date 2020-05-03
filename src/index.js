2540950147263ece7df4ac49c025baf1411a8b48
geonwoomun <ansejrrhkd@naver.com>
2020-05-03 11:50:56 +0900

    css까지 완료..?

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
 const dressList = document.querySelector('.dress-list');
 let dresses;
 
+function showAllList() { // 로고를 눌렀을 시 전체 리스트를 보여줌.
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click', () => {
+        renderList(dresses);
+    });    
+}
+
 async function loadData() { // json 파일로부터 데이터를 받아 오는 함수.
     const response = await fetch('./data.json');
     const data = await response.json();
@@ -18,7 +25,9 @@ function addFilterEvent(dresses) { // 필터 이벤트를 추가하는 함수
     filterList.addEventListener('click', (e) => {
         let filterSubject;
         let filterName;
-
+        if (e.target.tagName === 'UL') {
+            return;
+        }
         if (e.target.tagName === "IMG") {
             filterSubject = e.target.parentNode.className;
             filterName = e.target.parentNode.id;
@@ -37,22 +46,22 @@ function renderList (dresses) { // 옷들을 그리는 함수
     }
 
     dresses.forEach((dress) => {
-        const {gender, kinds, color, image} = dress;
+        const {gender, size,image} = dress;
         const li = document.createElement('li');
         const img = document.createElement('img');
         img.src = image;
         const span = document.createElement('span');
-        span.innerText = `${gender}, ${kinds}, ${color}`;
+        span.innerText = `${gender}, ${size} size`;
         li.appendChild(img);
         li.appendChild(span);
         dressList.appendChild(li);
     });
 }
 
-
 async function init() { 
     await loadData();
     addFilterEvent(dresses);
+    showAllList();
     renderList(dresses);
 }
 
